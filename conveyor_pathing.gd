ef42014b304578237518ce78c72bcf4632ca9581
Hamish Bell <hamish@lucid.com.au>
2025-08-26 10:36:37 +1000

    Added a very basic grey-box prototype scene
    - Biscuits travels along paths on the "conveyor belt".
    - Biscuits can be accepted or rejected. (Currently purely visual.)
    - Enter key toggles accept or reject state.
    - A lever visually communicates the accept/reject state.

---
+++ conveyor_pathing.gd
@@ -0,0 +1,47 @@
+class_name ConveyorPathing
+extends Node3D
+
+## A signal emitted when the course of the conveyor is changed.
+signal changed(product_rejected: bool)
+
+## The speed (in meters per second) of the conveyor.
+@export var conveyor_speed: float = 2.0
+
+## Whether products are currently being rejected.
+@export var reject_product: bool = false
+
+# The conveyor path components.
+@onready var base_path: Path3D = $BasePath
+@onready var accepted_path: Path3D = $AcceptedPath
+@onready var denied_path: Path3D = $DeniedPath
+
+var biscuit_pathers: Array[PathFollow3D]
+
+
+func _ready() -> void:
+	for child in base_path.get_children():
+		if child is PathFollow3D:
+			biscuit_pathers.append(child)
+
+
+func _process(delta: float) -> void:
+	for biscuit_pather in biscuit_pathers:
+		biscuit_pather.progress += conveyor_speed * delta
+		if biscuit_pather.progress_ratio >= 1.0:
+			# TODO: Matching the parent is probably not the most optimal choice.
+			#		Just temporary prototyping for now.
+			match biscuit_pather.get_parent():
+				base_path:
+					biscuit_pather.reparent(denied_path if reject_product else accepted_path)
+					biscuit_pather.progress = 0.0
+				accepted_path, denied_path:
+					# TODO: Handle biscuit reaching destination.
+					biscuit_pather.reparent(base_path)
+					biscuit_pather.progress = 0.0
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	# TODO: Temporary control
+	if event.is_action_pressed("ui_accept"):
+		reject_product = !reject_product
+		changed.emit(reject_product)
---
+++ lever.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@onready var conveyor_pathing: ConveyorPathing = $"../ConveyorPathing"
+@onready var handle: Node3D = $Handle
+
+var tween: Tween
+
+
+func _ready() -> void:
+	_on_conveyor_pathing_changed(conveyor_pathing.reject_product)
+
+
+func _on_conveyor_pathing_changed(product_rejected: bool) -> void:
+	const HANDLE_ACCEPT_POSITION: float = deg_to_rad(-30)
+	const HANDLE_REJECT_POSITION: float = deg_to_rad(30)
+	if tween:
+		tween.kill()
+	if product_rejected:
+		tween = create_tween()
+		tween.tween_property(handle, "rotation:x", HANDLE_REJECT_POSITION, 0.25).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	else:
+		tween = create_tween()
+		tween.tween_property(handle, "rotation:x", HANDLE_ACCEPT_POSITION, 0.25).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
