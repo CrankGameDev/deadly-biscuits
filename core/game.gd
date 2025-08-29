0f3facefbc884588cb184c033b52907a139e0fc9
ColdBloodedJellyDoughnut <154425002+ColdBloodedJellyDoughnut@users.noreply.github.com>
2025-08-29 17:21:57 +1000

    more ui
    i am healthy

---
+++ core/game.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+
+@onready var checklist: CanvasLayer = $Checklist
+var paused = false 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause"):
+		checklistMenu()
+#bring up screen with checklist
+func checklistMenu(): 
+	if paused:
+		checklist.hide() 
+		Engine.time_scale = 1
+	else:
+		checklist.show()
+		Engine.time_scale = 0
+		
+	paused = !paused 	
+		
+		
+	
